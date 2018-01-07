f955df5f362672b2e868de1e176eca285526db5b
qianwenyuan <14307130176@fudan.edu.cn>
2018-01-07 21:35:48 +0800

    最终版
    Signed-off-by: frankqian <14307130176@fudan.edu.cn>

--- src/test/drawtree.sh
+++ src/test/drawtree.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export CLASSPATH=".;./antlr-4.5-complete.jar:$CLASSPATH"
+export CLASSPATH=".:./antlr-4.5-complete.jar:$CLASSPATH"
 java -jar ./antlr-4.5-complete.jar MiniJava.g4
 javac MiniJava*.java
 java org.antlr.v4.runtime.misc.TestRig MiniJava goal factorial.java -gui
